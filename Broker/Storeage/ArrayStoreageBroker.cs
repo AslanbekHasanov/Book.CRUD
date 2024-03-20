a21a0add5902531dd54b7fdb871861060deb6efc
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-03-20 16:03:24 +0500

    Read Book

---
+++ Broker/Storeage/ArrayStoreageBroker.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using Book.CRUD.Models;
+
+namespace Book.CRUD.Broker.Storeage
+{
+    public class ArrayStoreageBroker : IStoreageBroker
+    {
+        private Books[] BooksInfo { get; set; } = new Books[10];
+
+        public ArrayStoreageBroker()
+        {
+            BooksInfo[0] = new Books()
+            {
+                Id = 1,
+                Name = "O'tgan kunlar",
+                Author = "Abdullo Qodiriy"
+            };
+            BooksInfo[1] = new Books()
+            {
+                Id = 2,
+                Name = "Shum bola",
+                Author = "G'ofur G'ulom"
+            };
+        }
+        public Books ReadBook(int id)
+        {
+            for(int itaration = 0; itaration <= BooksInfo.Length; itaration++)
+            {
+                Books bookInfoLine = BooksInfo[itaration];
+                if (bookInfoLine.Id == id)
+                {
+                    return bookInfoLine;
+                }
+            }
+
+            return new Books();
+        }
+    }
+
+}
---
+++ Service/IBookService.cs
@@ -0,0 +1,10 @@
+﻿using Book.CRUD.Models;
+
+namespace Book.CRUD.Service
+{
+    internal interface IBookService
+    {
+        Books GetBook(int id);
+        //ReadAllBook()
+    }
+}
