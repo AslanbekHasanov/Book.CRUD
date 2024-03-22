6039a97f80d0da199ba33752c4b23f34d1d373d7
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-03-22 16:07:44 +0500

    Add Book

--- Broker/Storeage/ArrayStoreageBroker.cs
+++ Broker/Storeage/ArrayStoreageBroker.cs
@@ -6,22 +6,22 @@ namespace Book.CRUD.Broker.Storeage
 {
     public class ArrayStoreageBroker : IStoreageBroker
     {
-        private Books[] BooksInfo { get; set; } = new Books[0];
+        private Books[] BooksInfo { get; set; } = new Books[10];
 
         public ArrayStoreageBroker()
         {
-            //BooksInfo[0] = new Books()
-            //{
-            //    Id = 1,
-            //    Name = "O'tgan kunlar",
-            //    Author = "Abdullo Qodiriy"
-            //};
-            //BooksInfo[1] = new Books()
-            //{
-            //    Id = 2,
-            //    Name = "Shum bola",
-            //    Author = "G'ofur G'ulom"
-            //};
+            BooksInfo[0] = new Books()
+            {
+                Id = 1,
+                Name = "O'tgan kunlar",
+                Author = "Abdullo Qodiriy"
+            };
+            BooksInfo[1] = new Books()
+            {
+                Id = 2,
+                Name = "Shum bola",
+                Author = "G'ofur G'ulom"
+            };
         }
         public Books ReadBook(int id)
         {
@@ -38,6 +38,25 @@ namespace Book.CRUD.Broker.Storeage
         }
 
         public Books[] GetAllBook() => BooksInfo;
+
+        public Books AddBook(Books book)
+        {
+            for (int itaration = 0; itaration < BooksInfo.Length; itaration++)
+            {
+                if (BooksInfo[itaration] is null)
+                {
+                    var bookInfo = new Books()
+                    {
+                        Id = book.Id,
+                        Name = book.Name,
+                        Author = book.Author
+                    };
+                    BooksInfo[itaration] = bookInfo;
+                    return book;
+                }
+            }
+            return new Books();
+        }
     }
 
 }
